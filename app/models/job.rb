555b92b71f21d4f483db39774bae7552c2871411
James Beavers <jamesjbeavers@gmail.com>
2014-10-08 20:51:14 -0400

    Re-ordered job field validations (priority) and remove deadline hint in view (going to make this more general)

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,9 +3,8 @@ class Job < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_one :category
 
-
-  validates :owner_id, :title, :description, :category_id, :deadline,  presence: true
   validate  :valid_date_format?
+  validates :owner_id, :title, :description, :category_id, :deadline,  presence: true
 
   # nil should be returned from controller if improperly formatted
   # Add to errors list if so
