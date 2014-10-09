f796ab93aab9a2b773e9d4463a9da14bd182c206
James Beavers <jamesjbeavers@gmail.com>
2014-10-09 00:54:52 -0400

    Correct migration column name

--- db/migrate/20140926213410_create_jobs_users.rb
+++ db/migrate/20140926213410_create_jobs_users.rb
@@ -2,7 +2,7 @@ class CreateJobsUsers < ActiveRecord::Migration
   def change
     create_table :jobs_users, :id => false do |t|
       t.belongs_to :user
-      t.belongs_to :jobs
+      t.belongs_to :job
     end
   end
 end
